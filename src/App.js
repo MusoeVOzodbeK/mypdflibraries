d8885c7b3de73aa2a63ba0bf4221a27fde4c759e
musoev_ozodbek <musoyev2004@samtuit.uz>
2023-04-25 22:16:17 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
+import Pdf from './kaktuslar.pdf'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="./pdf/S._Unsal_Kaktuslar_ham_gullaydi.pdf"
+          href={Pdf}
           target="_blank"
           rel="noopener noreferrer"
           download='Kakuslar ham gullaydi'
